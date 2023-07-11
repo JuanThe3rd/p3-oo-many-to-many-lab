4e148f652d9519828cc8b3da95a2cf88be279daf
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-07-10 19:50:06 -0700

    lab complete

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,65 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
 
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in Contract.all]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        count = 0
+
+        for contract in Contract.all: 
+            if contract.author == self:
+                count += contract.royalties
+
+        return count
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
 
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in Contract.all if contract.book == self]
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            raise Exception
+        if isinstance(book, Book):
+            self.book = book
+        else:
+            raise Exception
+        if isinstance(date, str):
+            self.date = date
+        else:
+            raise Exception
+        if isinstance(royalties, int):
+            self.royalties = royalties
+        else:
+            raise Exception
+        
+        Contract.all.append(self)
+
+    def contracts_by_date():
+        sorted_dates = sorted([contract.date for contract in Contract.all])
+        sorted_contracts = []
+
+        for date in sorted_dates:
+            for contract in Contract.all:
+                if contract.date == date:
+                    sorted_contracts.append(contract)
+
+        return sorted_contracts
